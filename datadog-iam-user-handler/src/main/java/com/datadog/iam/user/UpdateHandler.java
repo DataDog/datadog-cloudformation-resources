4093ddd193c97052d6ab4a8a04a305b384640bd8
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2019-08-27 15:11:46 +0200

    [User] Implement updateHandler for user resource

--- datadog-iam-user-handler/src/main/java/com/datadog/iam/user/UpdateHandler.java
+++ datadog-iam-user-handler/src/main/java/com/datadog/iam/user/UpdateHandler.java
@@ -6,6 +6,15 @@ import com.amazonaws.cloudformation.proxy.ProgressEvent;
 import com.amazonaws.cloudformation.proxy.OperationStatus;
 import com.amazonaws.cloudformation.proxy.ResourceHandlerRequest;
 
+import com.datadog.cloudformation.common.clients.ApiClients;
+
+import com.datadog.api.client.v1.ApiClient;
+import com.datadog.api.client.v1.ApiException;
+import com.datadog.api.client.v1.api.UsersApi;
+import com.datadog.api.client.v1.model.User;
+import com.datadog.api.client.v1.model.UserUpdatePayload;
+import com.datadog.api.client.v1.model.UserUpdateResponse;
+
 public class UpdateHandler extends BaseHandler<CallbackContext> {
 
     @Override
@@ -17,11 +26,31 @@ public class UpdateHandler extends BaseHandler<CallbackContext> {
 
         final ResourceModel model = request.getDesiredResourceState();
 
-        // TODO : put your code here
+        ApiClient apiClient = ApiClients.V1Client(
+            model.getDatadogCredentials().getApiKey(),
+            model.getDatadogCredentials().getApplicationKey()
+        );
+        UsersApi usersApi = new UsersApi(apiClient);
 
-        return ProgressEvent.<ResourceModel, CallbackContext>builder()
-            .resourceModel(model)
-            .status(OperationStatus.SUCCESS)
-            .build();
+        UserUpdatePayload userUpdatePayload = new UserUpdatePayload()
+            .accessRole(UserUpdatePayload.AccessRoleEnum.fromValue(model.getAccessRole()))
+            .disabled(model.getDisabled())
+            .email(model.getEmail())
+            .name(model.getName());
+
+        try {
+            usersApi.updateUser(model.getHandle(), userUpdatePayload);
+        } catch (ApiException e) {
+            String err = "Failed to update user: " + e.toString();
+            logger.log(err);
+
+            return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                .resourceModel(model)
+                .status(OperationStatus.FAILED)
+                .message(err)
+                .build();
+        }
+
+        return new ReadHandler().handleRequest(proxy, request, callbackContext, logger);
     }
 }

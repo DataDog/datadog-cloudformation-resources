e5d1e471155c3db762b3d0173a4bab1982febe0c
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-12-31 14:55:12 -0500

    Add license note at top of source files

--- datadog-cloudformation-common/src/test/java/com/datadog/cloudformation/utils/AppTest.java
+++ datadog-cloudformation-common/src/test/java/com/datadog/cloudformation/utils/AppTest.java
@@ -1,3 +1,6 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
 package com.datadog.cloudformation.utils;
 
 import junit.framework.Test;
@@ -7,7 +10,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
     extends TestCase
 {
     /**

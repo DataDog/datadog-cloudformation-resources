419643217fcb4702d55c69b3d9ce5ecbb294e028
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2020-10-19 12:01:58 +0200

    Create common python package with utils for developing resources (#92)

---
+++ datadog-cloudformation-common-python/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+version = {}
+with open("src/datadog_cloudformation_common/version.py") as fp:
+    exec(fp.read(), version)
+
+setup(
+    version=version["__version__"]
+)

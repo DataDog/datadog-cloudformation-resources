a6b45d2b5e4870abc180f88dedd82bb9914465e1
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2020-10-21 09:30:18 -0400

    Specify User Agent header and set monitor id on create (#94)
    * Add user agent header
    
    * Use string header
    
    * Set monitor id, and bump the commit for common
    
    * Add default for api_url

--- datadog-cloudformation-common-python/src/datadog_cloudformation_common/api_clients.py
+++ datadog-cloudformation-common-python/src/datadog_cloudformation_common/api_clients.py
@@ -6,14 +6,13 @@ from datadog_api_client.v1 import ApiClient, Configuration
 @contextmanager
 def v1_client(api_key: str, app_key: str, api_url: str, resource_name: str, resource_version: str) -> ApiClient:
     configuration = Configuration(
-        host=api_url,
+        host=api_url or "https://api.datadoghq.com",
         api_key={
             "apiKeyAuth": api_key,
             "appKeyAuth": app_key,
         }
     )
 
-    # TODO: configure user-agent
-
     with ApiClient(configuration) as api_client:
+        api_client.user_agent = f"datadog-cloudformation-{resource_name}/{resource_version} {api_client.user_agent}"
         yield api_client

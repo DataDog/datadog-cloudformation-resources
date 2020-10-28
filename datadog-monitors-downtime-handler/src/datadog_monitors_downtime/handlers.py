f6e57f47d699beca673467584fb181ba0d4eb726
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2020-10-28 11:57:00 -0400

    Start downtime migration

---
+++ datadog-monitors-downtime-handler/src/datadog_monitors_downtime/handlers.py
@@ -0,0 +1,148 @@
+import logging
+from typing import Any, MutableMapping, Optional
+
+from cloudformation_cli_python_lib import (
+    Action,
+    HandlerErrorCode,
+    OperationStatus,
+    ProgressEvent,
+    Resource,
+    SessionProxy,
+    exceptions,
+)
+from datadog_api_client.v1 import ApiException
+from datadog_api_client.v1.api import downtimes_api
+from datadog_api_client.v1.model.downtime import Downtime
+from datadog_cloudformation_common.api_clients import v1_client
+
+from .models import ResourceHandlerRequest, ResourceModel
+from .version import __version__
+
+# Use this logger to forward log messages to CloudWatch Logs.
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
+TYPE_NAME = "Datadog::Monitors::Downtime"
+TELEMETRY_TYPE_NAME = "monitors-downtime"
+
+resource = Resource(TYPE_NAME, ResourceModel)
+test_entrypoint = resource.test_entrypoint
+
+
+@resource.handler(Action.CREATE)
+def create_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    progress: ProgressEvent = ProgressEvent(
+        status=OperationStatus.IN_PROGRESS,
+        resourceModel=model,
+    )
+    LOG.info(f"Starting the {TYPE_NAME} Create Handler")
+
+    with v1_client(
+            model.DatadogCredentials.ApiKey,
+            model.DatadogCredentials.ApplicationKey,
+            model.DatadogCredentials.ApiURL,
+            TELEMETRY_TYPE_NAME,
+            __version__,
+    ) as api_client:
+        api_instance = downtimes_api.DowntimesApi(api_client)
+        # TODO Fill body
+        downtime_body = Downtime()
+        try:
+            api_resp = api_instance.create_downtime(downtime_body)
+            model.Id = api_resp.id
+        except ApiException as e:
+            LOG.error("Exception when calling DowntimeApi->create_downtime: %s\n" % e)
+            return ProgressEvent(status=OperationStatus.FAILED, resourceModel=model, message=e.body)
+
+    return read_handler(session, request, callback_context)
+
+
+@resource.handler(Action.UPDATE)
+def update_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    progress: ProgressEvent = ProgressEvent(
+        status=OperationStatus.IN_PROGRESS,
+        resourceModel=model,
+    )
+    # TODO: put code here
+    return progress
+
+
+@resource.handler(Action.DELETE)
+def delete_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    progress: ProgressEvent = ProgressEvent(
+        status=OperationStatus.IN_PROGRESS,
+        resourceModel=model,
+    )
+    # TODO: put code here
+    return progress
+
+
+@resource.handler(Action.READ)
+def read_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    LOG.info(f"Starting the {TYPE_NAME} Read Handler")
+
+    with v1_client(
+            model.DatadogCredentials.ApiKey,
+            model.DatadogCredentials.ApplicationKey,
+            model.DatadogCredentials.ApiURL,
+            TELEMETRY_TYPE_NAME,
+            __version__,
+    ) as api_client:
+        api_instance = downtimes_api.DowntimesApi(api_client)
+        try:
+            api_resp = api_instance.get_downtime(model.Id)
+        except ApiException as e:
+            LOG.error("Exception when calling DowntimeApi->get_downtime: %s\n" % e)
+            return ProgressEvent(status=OperationStatus.FAILED, resourceModel=model, message=e.body)
+
+    LOG.info(f"Success retrieving downtime {api_resp}")
+
+    # Not Nullable attributes
+    model.Message = api_resp.message
+    model.MonitorTags = api_resp.monitor_tags
+    model.Scope = api_resp.scope
+    model.Timezone = api_resp.timezone
+    model.Start = api_resp.start
+
+    # Nullable attributes
+    if api_resp.end:
+        model.End = api_resp.end
+    if api_resp.monitor_id:
+        model.MonitorId = api_resp.monitor_id
+
+    return ProgressEvent(
+        status=OperationStatus.SUCCESS,
+        resourceModel=model,
+    )
+
+
+@resource.handler(Action.LIST)
+def list_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    # TODO: put code here
+    return ProgressEvent(
+        status=OperationStatus.SUCCESS,
+        resourceModels=[],
+    )
---
+++ datadog-monitors-downtime-handler/src/datadog_monitors_downtime/models.py
@@ -0,0 +1,115 @@
+# DO NOT modify this file by hand, changes will be overwritten
+import sys
+from dataclasses import dataclass
+from inspect import getmembers, isclass
+from typing import (
+    AbstractSet,
+    Any,
+    Generic,
+    Mapping,
+    MutableMapping,
+    Optional,
+    Sequence,
+    Type,
+    TypeVar,
+)
+
+from cloudformation_cli_python_lib.interface import (
+    BaseModel,
+    BaseResourceHandlerRequest,
+)
+from cloudformation_cli_python_lib.recast import recast_object
+from cloudformation_cli_python_lib.utils import deserialize_list
+
+T = TypeVar("T")
+
+
+def set_or_none(value: Optional[Sequence[T]]) -> Optional[AbstractSet[T]]:
+    if value:
+        return set(value)
+    return None
+
+
+@dataclass
+class ResourceHandlerRequest(BaseResourceHandlerRequest):
+    # pylint: disable=invalid-name
+    desiredResourceState: Optional["ResourceModel"]
+    previousResourceState: Optional["ResourceModel"]
+
+
+@dataclass
+class ResourceModel(BaseModel):
+    DatadogCredentials: Optional["_DatadogCredentials"]
+    Active: Optional[bool]
+    Canceled: Optional[int]
+    CreatorId: Optional[int]
+    Disabled: Optional[bool]
+    DowntimeType: Optional[int]
+    End: Optional[int]
+    Id: Optional[int]
+    Message: Optional[str]
+    MonitorId: Optional[float]
+    MonitorTags: Optional[Sequence[str]]
+    ParentId: Optional[int]
+    Scope: Optional[Sequence[str]]
+    Start: Optional[int]
+    Timezone: Optional[str]
+    UpdaterId: Optional[int]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_ResourceModel"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_ResourceModel"]:
+        if not json_data:
+            return None
+        dataclasses = {n: o for n, o in getmembers(sys.modules[__name__]) if isclass(o)}
+        recast_object(cls, json_data, dataclasses)
+        return cls(
+            DatadogCredentials=DatadogCredentials._deserialize(json_data.get("DatadogCredentials")),
+            Active=json_data.get("Active"),
+            Canceled=json_data.get("Canceled"),
+            CreatorId=json_data.get("CreatorId"),
+            Disabled=json_data.get("Disabled"),
+            DowntimeType=json_data.get("DowntimeType"),
+            End=json_data.get("End"),
+            Id=json_data.get("Id"),
+            Message=json_data.get("Message"),
+            MonitorId=json_data.get("MonitorId"),
+            MonitorTags=json_data.get("MonitorTags"),
+            ParentId=json_data.get("ParentId"),
+            Scope=json_data.get("Scope"),
+            Start=json_data.get("Start"),
+            Timezone=json_data.get("Timezone"),
+            UpdaterId=json_data.get("UpdaterId"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_ResourceModel = ResourceModel
+
+
+@dataclass
+class DatadogCredentials(BaseModel):
+    ApiKey: Optional[str]
+    ApplicationKey: Optional[str]
+    ApiURL: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_DatadogCredentials"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_DatadogCredentials"]:
+        if not json_data:
+            return None
+        return cls(
+            ApiKey=json_data.get("ApiKey"),
+            ApplicationKey=json_data.get("ApplicationKey"),
+            ApiURL=json_data.get("ApiURL"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_DatadogCredentials = DatadogCredentials
+
+

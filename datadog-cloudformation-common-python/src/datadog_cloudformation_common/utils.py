cb6fe352f670fc49266782522cdd1cf5f4b86419
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2021-03-22 11:57:14 +0100

    Add util for http to handler error code (#126)

---
+++ datadog-cloudformation-common-python/src/datadog_cloudformation_common/utils.py
@@ -0,0 +1,23 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache 2.0 style license (see LICENSE).
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2020-Present Datadog, Inc.
+from cloudformation_cli_python_lib import HandlerErrorCode
+
+
+def http_to_handler_error_code(http_error_code: int) -> HandlerErrorCode:
+    if http_error_code == 400:
+        return HandlerErrorCode.InvalidRequest
+    elif http_error_code == 402:
+        return HandlerErrorCode.ServiceLimitExceeded
+    elif http_error_code == 403:
+        return HandlerErrorCode.AccessDenied
+    elif http_error_code == 404:
+        return HandlerErrorCode.NotFound
+    elif http_error_code == 409:
+        return HandlerErrorCode.ResourceConflict
+    elif http_error_code == 422:
+        return HandlerErrorCode.GeneralServiceException
+    elif http_error_code == 429:
+        return HandlerErrorCode.Throttling
+    return HandlerErrorCode.ServiceInternalError

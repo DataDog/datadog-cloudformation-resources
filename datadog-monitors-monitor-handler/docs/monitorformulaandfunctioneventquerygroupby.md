a250bca2143f38124a3b727d936413e381497332
skarimo <40482491+skarimo@users.noreply.github.com>
2023-01-05 14:57:27 -0500

    Add support for `variables` option (#237)
    * datadog-monitors-monitor-handler: add basic support for Options.Variables
    
    Co-authored-by: Niall Young <5465765+niallyoung@users.noreply.github.com>

---
+++ datadog-monitors-monitor-handler/docs/monitorformulaandfunctioneventquerygroupby.md
@@ -0,0 +1,56 @@
+# Datadog::Monitors::Monitor MonitorFormulaAndFunctionEventQueryGroupBy
+
+Credentials for the Datadog API
+
+## Syntax
+
+To declare this entity in your AWS CloudFormation template, use the following syntax:
+
+### JSON
+
+<pre>
+{
+    "<a href="#facet" title="Facet">Facet</a>" : <i>String</i>,
+    "<a href="#limit" title="Limit">Limit</a>" : <i>Integer</i>,
+    "<a href="#sort" title="Sort">Sort</a>" : <i><a href="monitorformulaandfunctioneventquerygroupby.md">MonitorFormulaAndFunctionEventQueryGroupBy</a></i>
+}
+</pre>
+
+### YAML
+
+<pre>
+<a href="#facet" title="Facet">Facet</a>: <i>String</i>
+<a href="#limit" title="Limit">Limit</a>: <i>Integer</i>
+<a href="#sort" title="Sort">Sort</a>: <i><a href="monitorformulaandfunctioneventquerygroupby.md">MonitorFormulaAndFunctionEventQueryGroupBy</a></i>
+</pre>
+
+## Properties
+
+#### Facet
+
+Event facet.
+
+_Required_: Yes
+
+_Type_: String
+
+_Update requires_: [No interruption](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-update-behaviors.html#update-no-interrupt)
+
+#### Limit
+
+Event facet.
+
+_Required_: No
+
+_Type_: Integer
+
+_Update requires_: [No interruption](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-update-behaviors.html#update-no-interrupt)
+
+#### Sort
+
+_Required_: No
+
+_Type_: <a href="monitorformulaandfunctioneventquerygroupby.md">MonitorFormulaAndFunctionEventQueryGroupBy</a>
+
+_Update requires_: [No interruption](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-update-behaviors.html#update-no-interrupt)
+
--- datadog-monitors-monitor-handler/src/datadog_monitors_monitor/models.py
+++ datadog-monitors-monitor-handler/src/datadog_monitors_monitor/models.py
@@ -132,6 +132,7 @@ class MonitorOptions(BaseModel):
     RenotifyStatuses: Optional[Sequence[str]]
     MinFailureDuration: Optional[int]
     NewGroupDelay: Optional[int]
+    Variables: Optional[Sequence["_MonitorFormulaAndFunctionEventQueryDefinition"]]
 
     @classmethod
     def _deserialize(
@@ -161,6 +162,7 @@ class MonitorOptions(BaseModel):
             RenotifyStatuses=json_data.get("RenotifyStatuses"),
             MinFailureDuration=json_data.get("MinFailureDuration"),
             NewGroupDelay=json_data.get("NewGroupDelay"),
+            Variables=deserialize_list(json_data.get("Variables"), MonitorFormulaAndFunctionEventQueryDefinition),
         )
 
 
@@ -216,6 +218,128 @@ class MonitorThresholdWindows(BaseModel):
 
 # work around possible type aliasing issues when variable has same name as a model
 _MonitorThresholdWindows = MonitorThresholdWindows
+
+
+@dataclass
+class MonitorFormulaAndFunctionEventQueryDefinition(BaseModel):
+    DataSource: Optional[str]
+    Search: Optional["_Search"]
+    Indexes: Optional[Sequence[str]]
+    Compute: Optional["_Compute"]
+    GroupBy: Optional[Sequence["_MonitorFormulaAndFunctionEventQueryGroupBy"]]
+    Name: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_MonitorFormulaAndFunctionEventQueryDefinition"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_MonitorFormulaAndFunctionEventQueryDefinition"]:
+        if not json_data:
+            return None
+        return cls(
+            DataSource=json_data.get("DataSource"),
+            Search=Search._deserialize(json_data.get("Search")),
+            Indexes=json_data.get("Indexes"),
+            Compute=Compute._deserialize(json_data.get("Compute")),
+            GroupBy=deserialize_list(json_data.get("GroupBy"), MonitorFormulaAndFunctionEventQueryGroupBy),
+            Name=json_data.get("Name"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_MonitorFormulaAndFunctionEventQueryDefinition = MonitorFormulaAndFunctionEventQueryDefinition
+
+
+@dataclass
+class Search(BaseModel):
+    Query: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_Search"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_Search"]:
+        if not json_data:
+            return None
+        return cls(
+            Query=json_data.get("Query"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_Search = Search
+
+
+@dataclass
+class Compute(BaseModel):
+    Aggregation: Optional[str]
+    Interval: Optional[int]
+    Metric: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_Compute"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_Compute"]:
+        if not json_data:
+            return None
+        return cls(
+            Aggregation=json_data.get("Aggregation"),
+            Interval=json_data.get("Interval"),
+            Metric=json_data.get("Metric"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_Compute = Compute
+
+
+@dataclass
+class MonitorFormulaAndFunctionEventQueryGroupBy(BaseModel):
+    Facet: Optional[str]
+    Limit: Optional[int]
+    Sort: Optional["_Sort"]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_MonitorFormulaAndFunctionEventQueryGroupBy"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_MonitorFormulaAndFunctionEventQueryGroupBy"]:
+        if not json_data:
+            return None
+        return cls(
+            Facet=json_data.get("Facet"),
+            Limit=json_data.get("Limit"),
+            Sort=Sort._deserialize(json_data.get("Sort")),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_MonitorFormulaAndFunctionEventQueryGroupBy = MonitorFormulaAndFunctionEventQueryGroupBy
+
+
+@dataclass
+class Sort(BaseModel):
+    Aggregation: Optional[str]
+    Metric: Optional[str]
+    Order: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_Sort"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_Sort"]:
+        if not json_data:
+            return None
+        return cls(
+            Aggregation=json_data.get("Aggregation"),
+            Metric=json_data.get("Metric"),
+            Order=json_data.get("Order"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_Sort = Sort
 
 
 @dataclass

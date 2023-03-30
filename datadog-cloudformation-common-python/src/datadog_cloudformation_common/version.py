8476f906fa2335af12753b1813c4630c88f541e5
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2023-03-30 10:11:12 -0400

    Prepare for common package 0.0.16 release

--- datadog-cloudformation-common-python/src/datadog_cloudformation_common/version.py
+++ datadog-cloudformation-common-python/src/datadog_cloudformation_common/version.py
@@ -2,4 +2,4 @@
 # under the Apache 2.0 style license (see LICENSE).
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2020-Present Datadog, Inc.
-__version__ = "0.0.15"
+__version__ = "0.0.16"
